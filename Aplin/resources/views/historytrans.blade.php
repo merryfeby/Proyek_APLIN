984b42b3470ccf8381bf44c854f7c5e548007592
Raphaelhans <rapha.hanley@gmail.com>
2024-05-15 23:16:14 +0700

    Data table

--- Aplin/resources/views/historytrans.blade.php
+++ Aplin/resources/views/historytrans.blade.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" /> -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+
     <script src="https://kit.fontawesome.com/b9b5e10605.js" crossorigin="anonymous"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
 
@@ -70,32 +72,19 @@
         </aside>
         <div class="main p-3">
             <h1>Transaction History</h1>
-            <!-- <div>
-                <h1>Add New Offer</h1>
-                <div class="register">
-                    <p>Offer code: <input type="text"></p>
-                    <p>Discount: <input type="number"></p>
-                    <p>Maximal Transaction: <input type="number"></p>
-                    <p>Expired Date: <input type="date"></p>
-                    <p>Details: <textarea name="" id="" cols="30" rows="3"></textarea></p>
-                    <br>
-                    <button type="button" class="btn btn-primary">Add</button>
-                </div>
-            </div> -->
-            <br>
-            <table class="table table-hover ">
-                <thead>
-                  <tr>
-                    <th>ID Nota</th>
-                    <th>ID Movie</th>
-                    <th>ID Kursi</th>
-                    <th>ID Studio</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                    <th>Date of Purchase</th>
-                    <th>Payment Method</th>
-                  </tr>
+
+            <table id="transactionTable" class="table table-hover table-bordered">  <thead>
+                    <tr>
+                        <th>ID Nota</th>
+                        <th>ID Movie</th>
+                        <th>ID Kursi</th>
+                        <th>ID Studio</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th>Date of Purchase</th>
+                        <th>Payment Method</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
@@ -108,7 +97,6 @@
                         <td>Rp. 100.000</td>
                         <td>2024-04-29</td>
                         <td>Q-Ris</td>
-                        
                     </tr>
                     <tr>
                         <td>2</td>
@@ -119,14 +107,32 @@
                         <td>2</td>
                         <td>Rp. 80.000</td>
                         <td>2024-04-29</td>
-                        <td>Q-Ris</td>                    </tr>
+                        <td>Q-Ris</td>
+                    </tr>
                 </tbody>
-              </table>
+            </table>
         </div>
     </div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 
     <script src="{{ asset('assets/js/script.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#transactionTable').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            }); 
+        });
+    </script>
 </body>
 
 </html>
