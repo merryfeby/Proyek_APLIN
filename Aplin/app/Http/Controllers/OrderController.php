7cc5d704e69baf2a563942f2e631713fda2082e6
Rinko7 <merryfebyanti@gmail.com>
2024-06-04 19:52:49 +0700

    update dikit

--- Aplin/app/Http/Controllers/OrderController.php
+++ Aplin/app/Http/Controllers/OrderController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     function index() {
         return view('user_site.detailBuyTicket');
+    }
+
+    function showDetail($id) {
+		$data = Movie::with('screening.studio')->find($id);
+        $studioName = $data->screening->first()->studio->name ?? 'No studio found';
+		return view('user_site.detailBuyTicket', compact('data', 'studioName'));
     }
 }
