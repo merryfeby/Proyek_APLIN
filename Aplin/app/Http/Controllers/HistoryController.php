fef4759785ab32c55af53a253a8fafdfd016a81a
Xyvern <cosmicbro08@gmail.com>
2024-06-05 23:23:28 +0700

    #27 Updated transaction history view
    Removed the hardcode and used the template to display data from database. Added changes to HistoryController, OrderController, and historyTicket & detailBuyTicket view

--- Aplin/app/Http/Controllers/HistoryController.php
+++ Aplin/app/Http/Controllers/HistoryController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\orderdetail;
+use App\Models\order;
 class HistoryController extends Controller
 {
     function index() {
-        return view('user_site.historyTicket');
-    }
+        $orders = Order::with('orderDetails.seat', 'screening.movie')
+            ->where('customer', session('login')->username)
+            ->get();
+        return view('user_site.historyTicket', ['orders' => $orders]);
+        }
 }
